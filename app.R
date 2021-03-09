be130d45a80fd6c0c5a7e9affa5fb5686d8e69ce
Arturo Prieto Tirado <52428590+apt345@users.noreply.github.com>
2021-03-09 12:40:15 +0100

    Added download report

--- app.R
+++ app.R
@@ -17,108 +17,133 @@ vaccines_countries = levels(vaccines$location)
 
 
 myHeader = div(id="advanced",
-               useShinyjs(),
-               selectInput(
-                 inputId = "selected_date",
-                 label = "Select the date(s)",
-                 multiple = TRUE,
-                 choices = vaccines_dates,
-                 selected = c(vaccines_dates[1])
-               ),
-               selectInput(
-                 inputId = "selected_location",
-                 label = "Select the location(s)",
-                 multiple = TRUE,
-                 choices = vaccines_countries,
-                 selected = c(vaccines_countries[1])
-               )
+                useShinyjs(),
+                selectInput(
+                    inputId = "selected_date",
+                    label = "Select the date(s)",
+                    multiple = TRUE,
+                    choices = vaccines_dates,
+                    selected = c(vaccines_dates[1])
+                ),
+                selectInput(
+                    inputId = "selected_location",
+                    label = "Select the location(s)",
+                    multiple = TRUE,
+                    choices = vaccines_countries,
+                    selected = c(vaccines_countries[1])
+                ),
+                downloadButton("report", "Generate report")
 )
 
 dataPanel = tabPanel("Data",
-                     tableOutput("data")
+                      tableOutput("data")
 )
 
 plotPanel = tabPanel("Plot",
-                     fluidRow(
-                       column(width = 8,
-                              plotOutput("plot",
-                                         hover = hoverOpts(id = "plot_hover", delayType = "throttle"),
-                              )),
-                       column(width = 4,
-                              h2("Info"),
-                              h3(
-                                textOutput("hoverlocation", container = span),
-                                textOutput("hoverdate", container = span),
-                              ),
-                              textOutput("hoverPop")
-                       )
-                     )
+                      fluidRow(
+                          column(width = 8,
+                                 plotOutput("plot",
+                                            hover = hoverOpts(id = "plot_hover", delayType = "throttle"),
+                                 )),
+                          column(width = 4,
+                                 h2("Info"),
+                                 h3(
+                                     textOutput("hoverlocation", container = span),
+                                     textOutput("hoverdate", container = span),
+                                 ),
+                                 textOutput("hoverPop")
+                          )
+                      )
 )
 
 plotlyPanel = tabPanel("Dynamic plot",
-                       plotly::plotlyOutput("plotly")
+                        plotly::plotlyOutput("plotly")
 )
 
 
 # Define UI for application 
 ui = navbarPage("Arturo's shiny App",
-                dataPanel,
-                plotPanel,
-                plotlyPanel,
-                header = myHeader,
-                theme = shinytheme("united"),
-                id = "navBar"
+                 dataPanel,
+                 plotPanel,
+                 plotlyPanel,
+                 header = myHeader,
+                 theme = shinytheme("superhero"),
+                 id = "navBar"
 )
 
 # Define server logic 
 server = function(input, output, session) { 
-  
-  vaccines_date = reactive({vaccines %>%
-      filter(date %in% input$selected_date, location %in% input$selected_location)})
-  
-  output$data = renderTable(vaccines_date());
-  
-  output$plot = renderPlot(
-    ggplot(vaccines_date(), aes(x=location, y=total_vaccinations, fill=date))
-    + geom_bar(stat="identity", position=position_dodge())
-  )
-  
-  output$plotly = plotly::renderPlotly(
-    ggplot(vaccines_date(), aes(x=location, y=total_vaccinations, fill=date))
-    + geom_bar(stat="identity", position=position_dodge())
-  )
-  
-  output$plot_hoverinfo = renderPrint({
-    cat("Hover (throttled):\n")
-    str(input$plot_hover)
-  })
-  
-  hoverlocationIdx = reactive({
-    req(input$plot_hover$x)
-    round(input$plot_hover$x)
-  })
-  hoverlocation = reactive({
-    req(hoverlocationIdx() > 0 & hoverlocationIdx() <= length(input$selected_location))
-    input$selected_location[hoverlocationIdx()]
-  })
-  hoverdateIdx = reactive(ceiling((input$plot_hover$x-hoverlocationIdx()+0.5)*length(input$selected_date)))
-  hoverdate = reactive({
-    req(input$plot_hover$x)
-    req(hoverlocation() != "")
-    input$selected_date[hoverdateIdx()]}
-  )
-  output$hoverlocation = renderText(hoverlocation())
-  output$hoverdate =renderText(hoverdate())
-  output$hoverPop =renderText(paste("Total vaccinations: ",
-                                    vaccines_date() %>% 
-                                      filter(date == hoverdate(), location == hoverlocation()) %>%
-                                      pull(total_vaccinations)))
-  #refresh page when moving through sections
-  observe({
-    shinyjs::show("advanced")
-  })
-  
-  
+    
+    vaccines_date = reactive({vaccines %>%
+            filter(date %in% input$selected_date, location %in% input$selected_location)})
+    
+    output$data = renderTable(vaccines_date());
+    
+    output$plot = renderPlot(
+        ggplot(vaccines_date(), aes(x=location, y=total_vaccinations, fill=date))
+        + geom_bar(stat="identity", position=position_dodge())
+    )
+    
+    output$plotly = plotly::renderPlotly(
+        ggplot(vaccines_date(), aes(x=location, y=total_vaccinations, fill=date))
+        + geom_bar(stat="identity", position=position_dodge())
+    )
+    
+    output$plot_hoverinfo = renderPrint({
+        cat("Hover (throttled):\n")
+        str(input$plot_hover)
+    })
+    
+    hoverlocationIdx = reactive({
+        req(input$plot_hover$x)
+        round(input$plot_hover$x)
+    })
+    hoverlocation = reactive({
+        req(hoverlocationIdx() > 0 & hoverlocationIdx() <= length(input$selected_location))
+        input$selected_location[hoverlocationIdx()]
+    })
+    hoverdateIdx = reactive(ceiling((input$plot_hover$x-hoverlocationIdx()+0.5)*length(input$selected_date)))
+    hoverdate = reactive({
+        req(input$plot_hover$x)
+        req(hoverlocation() != "")
+        input$selected_date[hoverdateIdx()]}
+    )
+    output$hoverlocation = renderText(hoverlocation())
+    output$hoverdate =renderText(hoverdate())
+    output$hoverPop =renderText(paste("Total vaccinations: ",
+                                       vaccines_date() %>% 
+                                           filter(date == hoverdate(), location == hoverlocation()) %>%
+                                           pull(total_vaccinations)))
+    #refresh page when moving through sections
+    observe({
+            shinyjs::show("advanced")
+    })
+    
+    output$report = downloadHandler(
+        # For PDF output, change this to "report.pdf"
+        filename = "report.pdf",
+        content = function(file) {
+            # Copy the report file to a temporary directory before processing it, in
+            # case we don't have write permissions to the current working dir (which
+            # can happen when deployed).
+            tempReport = file.path(tempdir(), "report.Rmd")
+            file.copy("report.Rmd", tempReport, overwrite = TRUE)
+            
+            # Set up parameters to pass to Rmd document
+            params = list(
+                selected_date = isolate(input$selected_date),
+                selected_location = isolate(input$selected_location)
+            )
+            
+            # Knit the document, passing in the `params` list, and eval it in a
+            # child of the global environment (this isolates the code in the document
+            # from the code in this app).
+            rmarkdown::render(tempReport, output_file = file,
+                              params = params,
+                              envir = new.env(parent = globalenv())
+            )
+        }
+    )
 }
 
 # Run the application 
